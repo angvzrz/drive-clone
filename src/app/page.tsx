a094ef0d98f2d5d5dd8a75b8d5b938af4fa5c74b
Angel Valdez <ang_vzrz@live.com.mx>
2025-03-10 17:39:36 -0600

    feat: render folder and files in dynamic route page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,7 @@
-import { Upload } from "lucide-react";
-import { FilesTable } from "@/components/FilesTable/files-table";
-import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
-import db from "@/lib/db";
-
 export default async function Home() {
-  const folders = await db.folder.findMany();
-  const files = await db.file.findMany();
-
   return (
     <main className="min-h-screen p-8">
-      <section className="flex justify-between py-4">
-        <h2 className="text-xl font-semibold tracking-tight">My Files</h2>
-        <Button
-          variant="secondary"
-          className={cn(
-            "cursor-pointer",
-            "rounded-2xl",
-            "bg-linear-[128.84deg,#0f6cbd_20.46%,#3c45ab_72.3%]",
-            "hover:bg-linear-[128.84deg,#025caa_20.46%,#222b91_72.3%]",
-          )}
-        >
-          <Upload />
-          Upload
-        </Button>
-      </section>
-      <FilesTable folders={folders} files={files} />
+      <p>Home Page</p>
     </main>
   );
 }
