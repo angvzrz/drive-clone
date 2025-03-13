cbf2b6b510d04dc81f378cf3b6b6b6bf5141edb4
Angel Valdez <ang_vzrz@live.com.mx>
2025-03-13 17:59:02 -0600

    refactor: separate header

--- src/app/folder/[folderId]/page.tsx
+++ src/app/folder/[folderId]/page.tsx
@@ -1,8 +1,6 @@
-import { Upload } from "lucide-react";
+import { FilesHeader } from "@/components/FilesHeader/files-header";
 import { FilesTable } from "@/components/FilesTable/files-table";
 import { QUERIES } from "@/server/db/queries";
-import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
 import { PageProps } from "@/types";
 
 export default async function DriveClone({ params }: PageProps) {
@@ -22,21 +20,7 @@ export default async function DriveClone({ params }: PageProps) {
 
   return (
     <main className="min-h-screen p-8">
-      <section className="flex justify-between py-4">
-        <h2 className="text-xl font-semibold tracking-tight">My Files</h2>
-        <Button
-          variant="secondary"
-          className={cn(
-            "cursor-pointer",
-            "rounded-2xl",
-            "bg-linear-[128.84deg,#0f6cbd_20.46%,#3c45ab_72.3%]",
-            "hover:bg-linear-[128.84deg,#025caa_20.46%,#222b91_72.3%]",
-          )}
-        >
-          <Upload />
-          Upload
-        </Button>
-      </section>
+      <FilesHeader />
       <FilesTable folders={folders} files={files} />
     </main>
   );
