c0e961a085faf8fb06e3924808952ef5e38513e4
Angel Valdez <ang_vzrz@live.com.mx>
2025-03-28 14:46:06 -0600

    feat: add function that builds a folder structure based in uploaded files

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,11 @@
-import type { Params } from "next/dist/server/request/params";
-import type { ReadonlyURLSearchParams } from "next/navigation";
+import type { Params } from 'next/dist/server/request/params';
+import type { ReadonlyURLSearchParams } from 'next/navigation';
 
 export type PageProps = {
   params: Promise<Params>;
   searchParams: Promise<ReadonlyURLSearchParams>;
+};
+
+export type FolderNode = {
+  [key: string]: File | FolderNode;
 };
