fe47b28ab126bbe4c194e653249ecd85008d1eac
Angel Valdez <ang_vzrz@live.com.mx>
2025-03-31 12:05:12 -0600

    feat: add directory attributes to input element

--- src/types/index.ts
+++ src/types/index.ts
@@ -9,3 +9,11 @@ export type PageProps = {
 export type FolderNode = {
   [key: string]: File | FolderNode;
 };
+
+declare module 'react' {
+  interface InputHTMLAttributes<T> extends HTMLAttributes<T> {
+    // extends React's HTMLAttributes
+    directory?: string;
+    webkitdirectory?: string;
+  }
+}
