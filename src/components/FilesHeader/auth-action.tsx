246f42396fbf3d745b18cf3687348a9b18f4f3b6
Angel Valdez <ang_vzrz@live.com.mx>
2025-03-31 18:02:11 -0600

    refactor: replace upload button with dropdown

--- src/components/FilesHeader/auth-action.tsx
+++ src/components/FilesHeader/auth-action.tsx
@@ -1,26 +1,7 @@
 'use client';
 
-import { useRouter, useParams } from 'next/navigation';
-import { DriveLoader, UploadButton } from '../common';
+import { DriveLoader, UploadDropdown } from '../common';
 import { Button } from '../ui/button';
-import { uploadFolder } from '@/server/actions';
-
-const handleFolderUpload = async (
-  event: React.ChangeEvent<HTMLInputElement>,
-  parentFolderId: number,
-) => {
-  const files = event.target.files;
-
-  if (!files) return;
-
-  const filesData = new FormData();
-  Array.from(files).forEach((file) => {
-    filesData.append('files', file);
-    filesData.append('relativePath', file.webkitRelativePath);
-  });
-
-  uploadFolder(filesData, parentFolderId);
-};
 
 interface AuthActionProps {
   isGettingUser: boolean;
@@ -33,9 +14,6 @@ export function AuthAction({
   isAuthenticated,
   onSigningIn,
 }: AuthActionProps) {
-  const navigate = useRouter();
-  const params = useParams<{ folderId: string }>();
-
   if (isGettingUser) {
     return <DriveLoader />;
   }
@@ -43,19 +21,7 @@ export function AuthAction({
   return (
     <>
       {isAuthenticated ? (
-        <>
-          <UploadButton
-            onUpload={() => navigate.refresh()}
-            folderId={Number(params.folderId)}
-          />
-          <input
-            type="file"
-            dir=""
-            webkitdirectory=""
-            multiple
-            onChange={(e) => handleFolderUpload(e, Number(params.folderId))}
-          />
-        </>
+        <UploadDropdown />
       ) : (
         <p className="flex items-center gap-4">
           Try as guest
